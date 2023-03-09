343f46e525e8e9474b8cce605dd83a1237c7565e
Tammi Nyberg <tamminyberg@gmail.com>
2023-03-09 16:45:32 +0100

    added rotation to cylinder

--- VersionControlTest/Assets/Scripts/Rotate.cs
+++ VersionControlTest/Assets/Scripts/Rotate.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Rotate : MonoBehaviour
 {
+     float speed = 50.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,6 @@ public class Rotate : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Rotate(Vector3.right * speed * Time.deltaTime);
     }
 }
